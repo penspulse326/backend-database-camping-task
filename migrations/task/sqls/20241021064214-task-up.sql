e07869038fc4ae39b2b40438b88a821f12464e18
SHIN <akiyachiumi@gmail.com>
2024-11-30 18:30:11 +0800

    feat: answer 6-4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -537,6 +537,15 @@
 
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
+    SELECT
+        SUM(price_paid) AS "總營收"
+    FROM
+        "CREDIT_PURCHASE" cp
+    WHERE
+        cp.purchase_at >= '2024-11-01 00:00:00'
+        AND cp.purchase_at <= '2024-11-30 23:59:59'
+    ;
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
+
