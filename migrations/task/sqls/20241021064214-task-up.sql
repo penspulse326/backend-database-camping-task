dedd6454cee5f7de4ff6a0b14a1989a6b312f7b1
SHIN <akiyachiumi@gmail.com>
2024-11-30 16:14:10 +0800

    feat: answer 5-2

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -317,10 +317,10 @@ WHERE
                 "USER"
             WHERE
                 email = 'wXlTq@hexschooltest.io'
-        ),
-        c.id,
-        '2024-11-24 16:00:00',
-        '即將授課'
+        ) AS user_id,
+        c.id AS course_id,
+        '2024-11-24 16:00:00' AS booking_at,
+        '即將授課' AS status
     FROM
         "USER" u
         LEFT JOIN "COURSE" c ON u.id = c.user_id
@@ -342,10 +342,10 @@ WHERE
                 "USER"
             WHERE
                 email = 'richman@hexschooltest.io'
-        ),
-        c.id,
-        '2024-11-24 16:00:00',
-        '即將授課'
+        ) AS user_id,
+        c.id AS course_id,
+        '2024-11-24 16:00:00' AS booking_at,
+        '即將授課' AS status
     FROM
         "USER" u
         LEFT JOIN "COURSE" c ON u.id = c.user_id
@@ -355,6 +355,28 @@ WHERE
 -- 5-2. 修改：`王小明`取消預約 `李燕容` 的課程，請在`COURSE_BOOKING`更新該筆預約資料：
     -- 1. 取消預約時間`cancelled_at` 設為2024-11-24 17:00:00
     -- 2. 狀態`status` 設定為課程已取消
+    UPDATE "COURSE_BOOKING"
+    SET
+        cancelled_at = '2024-11-24 17:00:00',
+        status = '課程已取消'
+    WHERE
+        user_id = (
+            SELECT
+                id
+            FROM
+                "USER"
+            WHERE
+                email = 'wXlTq@hexschooltest.io'
+        )
+        AND course_id IN (
+            SELECT
+                c.id
+            FROM
+                "USER" u
+                LEFT JOIN "COURSE" c ON u.id = c.user_id
+            WHERE
+                u.email = 'lee2000@hexschooltest.io'
+        )
 
 -- 5-3. 新增：`王小明`再次預約 `李燕容`   的課程，請在`COURSE_BOOKING`新增一筆資料：
     -- 1. 預約人設為`王小明`
