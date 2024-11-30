aac1e1ebd913e918ac06ad8c1095374f3124f0f2
SHIN <akiyachiumi@gmail.com>
2024-11-30 14:26:31 +0800

    feat: answer 3-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -236,7 +236,33 @@ VALUES
 
 -- 3-3 修改：更新教練的經驗年數，資料需求如下：
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
+    UPDATE "COACH"
+    SET
+        experience_years = 3
+    WHERE
+        user_id = (
+            SELECT
+                id
+            FROM
+                "USER"
+            WHERE
+                email = 'muscle@hexschooltest.io'
+        );
+
+
     -- 2. 教練`Q太郎` 的經驗年數為5年
+    UPDATE "COACH"
+    SET
+        experience_years = 5
+    WHERE
+        user_id = (
+            SELECT
+                id
+            FROM
+                "USER"
+            WHERE
+                email = 'starplatinum@hexschooltest.io'
+        );
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
 
