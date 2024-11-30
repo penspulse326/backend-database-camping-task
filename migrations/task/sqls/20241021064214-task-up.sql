1cf36c4bd42aa416f3272ccc05c64d23cc762918
SHIN <akiyachiumi@gmail.com>
2024-11-30 12:22:28 +0800

    feat: answer 1-5

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -7,7 +7,7 @@
 -- ===================== ====================
 -- 1. 用戶資料，資料表為 USER
 
--- 1. 新增：新增六筆用戶資料，資料如下：
+-- 1-1 新增：新增六筆用戶資料，資料如下：
 --     1. 用戶名稱為`李燕容`，Email 為`lee2000@hexschooltest.io`，Role為`USER`
 --     2. 用戶名稱為`王小明`，Email 為`wXlTq@hexschooltest.io`，Role為`USER`
 --     3. 用戶名稱為`肌肉棒子`，Email 為`muscle@hexschooltest.io`，Role為`USER`
@@ -25,6 +25,7 @@ VALUES
   ('Q太郎', 'starplatinum@hexschooltest.io', 'USER'),
   ('透明人', 'opacity0@hexschooltest.io', 'USER');
 
+
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 
 UPDATE "USER"
@@ -32,11 +33,13 @@ SET ROLE = 'COACH'
 WHERE
 	email IN ('lee2000@hexschooltest.io', 'muscle@hexschooltest.io', 'starplatinum@hexschooltest.io');
 
+
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
 
 DELETE FROM "USER"
 WHERE
 	email = 'opcatiy0@hexschooltest.io';
+
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
 
@@ -45,7 +48,15 @@ SELECT
 FROM
 	"USER";
 
+
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
+
+SELECT
+	*
+FROM
+	"USER"
+LIMIT
+	3;
 
 
 --  ████████  █████   █    ████  
