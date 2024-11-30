7e41b615ec8f8901a50010f2ffa2288724736e1c
SHIN <akiyachiumi@gmail.com>
2024-11-30 18:31:47 +0800

    feat: answer 6-5

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -548,4 +548,12 @@
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
-
+    SELECT
+        COUNT(DISTINCT cb.user_id) AS "預約會員人數"
+    FROM
+        "COURSE_BOOKING" cb
+    WHERE
+        cb.created_at >= '2024-11-01 00:00:00'
+        AND cb.created_at <= '2024-11-30 23:59:59'
+        AND cb.status NOT IN ('課程已取消')
+    ;
