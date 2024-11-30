d1007096e0b6b7abad526c55aa33c8c8d90d5498
SHIN <akiyachiumi@gmail.com>
2024-11-30 14:28:16 +0800

    feat: answer 3-4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -265,6 +265,13 @@ VALUES
         );
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
+INSERT INTO
+	"SKILL" (name)
+VALUES
+	('空中瑜伽')
+DELETE FROM "SKILL"
+WHERE
+	name = '空中瑜伽';
 
 
 --  ████████  █████   █    █   █ 
