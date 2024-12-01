797fe14efd58a5e52934e9b2e596eafc8fffc99a
SHIN <akiyachiumi@gmail.com>
2024-12-01 20:18:53 +0800

    fixed: answer 6-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -543,8 +543,7 @@
         "CREDIT_PURCHASE" cp
     WHERE
         cp.purchase_at >= '2024-11-01 00:00:00'
-        AND cp.purchase_at <= '2024-11-30 23:59:59'
-    ;
+        AND cp.purchase_at <= '2024-11-30 23:59:59';
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
@@ -555,5 +554,4 @@
     WHERE
         cb.created_at >= '2024-11-01 00:00:00'
         AND cb.created_at <= '2024-11-30 23:59:59'
-        AND cb.status NOT IN ('課程已取消')
-    ;
+        AND cb.status NOT IN ('課程已取消');
